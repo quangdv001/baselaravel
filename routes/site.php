8321b3d43485fb5b1c344daea02c5b1c62a0ca10
Vinh Quang <quangdv001@gmail.com>
2019-07-08 21:21:39 +0700

    fix

--- routes/site.php
+++ routes/site.php
@@ -6,9 +6,15 @@ Route::post('login', 'SiteAuthController@postLogin')->name('auth.postLogin');
 Route::get('logout', 'SiteAuthController@logout')->name('auth.logout');
 
 Route::get('/', 'SiteHomeController@index')->name('home.index');
-Route::post('sendContact', 'SiteHomeController@sendContact')->name('home.sendContact');
 Route::get('lang/{locale}', 'SiteHomeController@lang')->name('home.setLocale');
 Route::get('currentLang', 'SiteArticleController@currentLang')->name('home.sendContact');
+Route::post('sendContact', 'SiteHomeController@sendContact')->name('home.sendContact');
+Route::get('about', 'SiteArticleController@about')->name('home.about');
+Route::get('price', 'SiteArticleController@price')->name('home.price');
+Route::get('livitrans', 'SiteArticleController@livitrans')->name('home.livitrans');
+Route::get('contact', 'SiteArticleController@contact')->name('home.contact');
+
+
 
 Route::get('article/i-{id}/{slug}', 'SiteArticleController@index')->name('article.index');
 Route::get('article/l-{id}/{slug}', 'SiteArticleController@list')->name('article.list');
