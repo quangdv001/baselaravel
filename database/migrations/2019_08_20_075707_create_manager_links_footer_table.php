641c8cd12f64182d5f5d873b2613ea6a6d249e2b
Thanhpk <pkthanhphp1991@gmail.com>
2019-08-20 14:58:27 +0700

    push

---
+++ database/migrations/2019_08_20_075707_create_manager_links_footer_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateManagerLinksFooterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manager_links_footer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('meta')->nullable();
+            $table->text('content')->nullable();
+            $table->smallInteger('status')->nullable()->default(1);
+            $table->integer('category_id')->nullable()->default(0);
+            $table->integer('user_id_c')->nullable()->default(0);
+            $table->string('user_name_c')->nullable();
+            $table->integer('user_id_u')->nullable()->default(0);
+            $table->string('user_name_u')->nullable();
+            $table->integer('admin_id_c')->nullable()->default(0);
+            $table->string('admin_name_c')->nullable();
+            $table->integer('admin_id_u')->nullable()->default(0);
+            $table->string('admin_name_u')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manager_links_footer');
+    }
+}
