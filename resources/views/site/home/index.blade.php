ae9d2d004e85fe01f8c5347943cbc20542b22d70
thanhpk <pkthanhphp1991@gmail.com>
2020-01-10 17:28:20 +0700

    fix trang chu

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -205,15 +205,15 @@ home
             </div>
         </div>
     </div>
-    @if count(advertise_horizontal > 0)
+    @if count($advertise_horizontal > 0)
     <div class="section-gap banner-text"
         style="    background: #ccc url(https://syntec-numerique.fr/sites/default/files/styles/medium/public/Image/finance-syntec-02-02-2017-2.jpg?itok=sXt3wLLF) no-repeat center top / 100% 600%;">
         <div class="container">
             <div class="banner-slide">
                 <div class="slide-owl-carousel owl-carousel owl-theme">
                     @foreach ($advertise_horizontal as $key_adv=>$item_adv)
-                    <a href="{{ item_adv->url }}">
-                        <img src="{{ item_adv->img}}" alt="1" style="height: 120px" />
+                    <a href="{{ $item_adv->url }}">
+                        <img src="{{ $item_adv->img}}" alt="1" style="height: 120px" />
                     </a>
                 </div>
             </div>
