1a23a74daff56c482ed9414104655ccd1ee91f98
quangdv <quangdovinh@vccorp.vn>
2020-03-11 15:15:32 +0700

    Fix

--- app/Http/Controllers/My/MyBillController.php
+++ app/Http/Controllers/My/MyBillController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\My;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Services\BillService;
+use App\Services\ContractService;
+use App\Services\CustomerService;
 
 class MyBillController extends Controller
 {
-    //
+    private $contract;
+    private $customer;
+    private $bill;
+    public function __construct(ContractService $contract, CustomerService $customer, BillService $bill)
+    {
+        $this->contract = $contract;
+        $this->customer = $customer;
+        $this->bill = $bill;
+    }
+
+    public function getCreate($id = 0){
+        $user = auth()->user();
+        $data = [];
+        if($id > 0){
+            $data = $this->contract->first(['id' =>$id, 'user_id' => $user->id]);
+            if(!$data){
+                return redirect()->route('my.contract.getList');
+            }
+        }
+        $customer = $this->customer->get(['user_id' => $user->id]);
+        return view('my.contract.edit')
+            ->with('id', $id)
+            ->with('data', $data);
+    }
 }
